d526dc05c14ebe67142436a3b0329e69b56443c2
Danish Anis <danishanis10@hotmail.com>
2024-10-23 20:18:19 -0400

    model evaluation

--- evaluate/evaluate_model.py
+++ evaluate/evaluate_model.py
@@ -3,9 +3,35 @@ import argparse
 import traceback
 from utils.model_helper import get_model
 
-
 def main():
     print("evaluating model...")
+
+exp = run.experiment
+ws = run.experiment.workspace
+run_id = 'amlcompute'
+
+parser = argparse.ArgumentParser("evaluate")
+
+parser.add_argument(
+    "--run_id",
+    type=str,
+    help="Training run ID",
+)
+parser.add_argument(
+    "--model_name",
+    type=str,
+    help="Name of the Model",
+    default="diabetes_model.pkl",
+)
+
+parser.add_argument(
+    "--allow_run_cancel",
+    type=str,
+    help="Set this to false to avoid evaluation step from cancelling run after an unsuccessful evaluation",  # NOQA: E501
+    default="true",
+)
+
+args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
