0830b74b16c7ef54ff05ad0186c8673fdfa34e69
Danish Anis <danishanis10@hotmail.com>
2024-10-23 20:16:30 -0400

    model evaluation

---
+++ evaluate/evaluate_model.py
@@ -0,0 +1,11 @@
+from azureml.core import Run
+import argparse
+import traceback
+from utils.model_helper import get_model
+
+
+def main():
+    print("evaluating model...")
+
+if __name__ == '__main__':
+    main()
